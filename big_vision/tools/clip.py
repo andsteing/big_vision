7ecc8eb0888ba9cf6e71dd5e8829f79942fd68ba
Andreas Steiner <andstein@google.com>
2022-10-03 10:46:58 +0200

    Makes prefix check a bit more stable.

--- big_vision/tools/clip.py
+++ big_vision/tools/clip.py
@@ -137,7 +137,8 @@ def load(name: str, device: Union[str, torch.device] = "cuda" if torch.cuda.is_a
   model = bv_torch.TwoTowers(**config)
   with open(path, 'rb') as f:
     w = _LoggingDict(**np.load(path))
-  model.load(w, prefix='params/' if next(iter(w)).startswith('params/') else '')
+  prefixes = {k.split('/')[0] for k in w}
+  model.load(w, prefix='params/' if 'params' in prefixes else '')
   unused = set(w).difference(w.accessed)
   assert not unused, unused
   overused = {k: v for k, v in w.accessed.items() if v > 1}
